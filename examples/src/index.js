e1c84a67ebe6d504271b67a8937a2a9a92b71edf
Basim Hennawi <basim.hennawi@figo.io>
2018-05-22 16:20:56 +0200

    Structure setup

---
+++ examples/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render} from 'react-dom';
+import ChipTAN from '../../src';
+
+const App = () => (
+    <ChipTAN />
+);
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const ChipTAN = () => (
+    <h1>Hello from ChipTAN component</h1>
+);
+
+export default ChipTAN;
