a17128d3da747cb8257537aeb13af569e5659189
Basim Hennawi <basim.hennawi@figo.io>
2018-05-23 11:41:36 +0200

    Refactor flicker logic

--- examples/src/index.js
+++ examples/src/index.js
@@ -5,8 +5,8 @@ import ChipTAN from '../../src';
 const App = () => (
     <ChipTAN
         data={'17850120452019980412345678041234567804123456789E'}
-        width={400}
-        height={200}
+        width={200}
+        height={100}
         bgColor='#000000'
         barColor='#ffffff'
     />
